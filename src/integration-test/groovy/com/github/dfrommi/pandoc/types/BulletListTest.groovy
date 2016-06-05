f4b7f312a047a14ffe42146cea5bddde4dcf57d5
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2016-06-05 14:25:29 +0200

    added test for nested lists

--- src/integration-test/groovy/com/github/dfrommi/pandoc/types/BulletListTest.groovy
+++ src/integration-test/groovy/com/github/dfrommi/pandoc/types/BulletListTest.groovy
@@ -6,21 +6,32 @@ class BulletListTest extends TypeTestBase {
 	@Parameters
 	static getParams() { [
 		[
-'''
-* one
-* two
-''',
-		new BulletList(items: [
-			[new Plain(content: new Str("one"))],
-			[new Plain(content: new Str("two"))]
-		])] as Object[], [
+//'''
+//* one
+//* two
+//''',
+//		new BulletList(items: [
+//			[new Plain(content: new Str("one"))],
+//			[new Plain(content: new Str("two"))]
+//		])] as Object[], [
+//'''
+//- one
+//- two
+//''',
+//			new BulletList(items: [
+//				[new Plain(content: new Str("one"))],
+//				[new Plain(content: new Str("two"))]
+//			])] as Object[], [
 '''
 - one
-- two
+    - two
 ''',
 			new BulletList(items: [
-				[new Plain(content: new Str("one"))],
-				[new Plain(content: new Str("two"))]
+					[new Plain(content: new Str("one")),
+					 new BulletList(items: [
+							[new Plain(content: new Str("two"))]
+
+					])]
 			])] as Object[]
-	] }
+		] }
 }
